55ac8dca3f840b08f6244ad1a0bb1f777800702c
Rodion Sibov <rodionsibov@gmail.com>
2022-04-28 20:40:54 +0300

    update

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 import { QuestionService } from '../services/question.service';
 
 @Component({
@@ -12,6 +13,9 @@ export class QuestionComponent implements OnInit {
   currentQuestion: number = 0;
   points: number = 0;
   counter: number = 60;
+  correctAnswer: number = 0;
+  inCorrectAnswer: number = 0;
+  interval$: any;
 
   constructor(private questionService: QuestionService) {}
 
@@ -34,6 +38,40 @@ export class QuestionComponent implements OnInit {
     this.currentQuestion--;
   }
 
-  
+  answer(currentQuestion: number, option: any): void {
+    if (option.correct) {
+      this.points += 10;
+      this.correctAnswer++;
+      this.currentQuestion++;
+    } else {
+      this.points -= 10;
+      this.inCorrectAnswer++;
+      this.currentQuestion++;
+    }
+  }
 
+  startCounter(): void {
+    this.interval$ = interval(1000).subscribe((val) => {
+      this.counter--;
+      if (this.counter === 0) {
+        this.currentQuestion++;
+        this.counter = 60;
+        this.points -= 10;
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe();
+    }, 600000);
+  }
+
+  stopCounter(): void {
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+
+  resetCounter(): void {
+    this.stopCounter();
+    this.counter = 60;
+    this.startCounter();
+  }
 }
