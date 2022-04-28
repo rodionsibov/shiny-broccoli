fcbaaa02bc6cae2f69a12c0c806e7e32c10acdfc
Rodion Sibov <rodionsibov@gmail.com>
2022-04-28 17:30:57 +0300

    add questions json

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+  name: string = ''
 
   constructor() { }
 
   ngOnInit(): void {
+    this.name = localStorage.getItem('name')!
   }
 
 }
