207137a2dcdc441edd318fcebe0448623b77a5a6
Rodion Sibov <rodionsibov@gmail.com>
2022-04-28 19:24:06 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +17,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../services/question.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
 })
 export class QuestionComponent implements OnInit {
-  name: string = ''
+  name: string = '';
+  questionList: any = [];
+  currentQuestion: number = 0;
+  points: number = 0;
+  counter: number = 60;
 
-  constructor() { }
+  constructor(private questionService: QuestionService) {}
 
   ngOnInit(): void {
-    this.name = localStorage.getItem('name')!
+    this.name = localStorage.getItem('name')!;
+    this.getAllQuestions();
   }
 
+  getAllQuestions(): void {
+    this.questionService.getQuestion().subscribe((res: any) => {
+      this.questionList = res.questions;
+    });
+  }
+
+  nextQuestion(): void {
+    this.currentQuestion++;
+  }
+
+  previousQuestion(): void {
+    this.currentQuestion--;
+  }
 }
