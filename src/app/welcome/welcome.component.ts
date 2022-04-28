7fc4007233d32438c544b5ee4355ac1ceb5e338e
Rodion Sibov <rodionsibov@gmail.com>
2022-04-28 17:23:49 +0300

    update

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  @ViewChild('name') nameKey!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startQuiz(): void {
+    localStorage.setItem('name', this.nameKey.nativeElement.value);
   }
-
 }
